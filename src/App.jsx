bb2ee86893807b987092d7d816688a641a2494f3
StanleywChen <stanleywchen@outlook.com>
2023-08-05 18:38:48 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SearchForm from "./SearchForm";
+import ThemeToggle from "./ThemeToggle";
+import Gallery from "./Gallery";
+
+const App = () => {
+  return <main>
+    <ThemeToggle />
+    <SearchForm />
+    <Gallery />
+  </main>;
+};
+export default App;
